ffc2ed02e823b4810293be23ff0e993e1500b5b8
Craig Walls <craig@habuma.com>
2018-02-06 22:30:50 -0700

    Build against Boot 2.0 and reinstate tests.

--- spring-skills/src/test/java/spring/skills/IntentDispatcherTests.java
+++ spring-skills/src/test/java/spring/skills/IntentDispatcherTests.java
@@ -15,135 +15,155 @@
  */
 package spring.skills;
 
+import static org.junit.Assert.assertEquals;
+import static spring.skills.SpringSkillsTestHelpers.buildTestRequestEnvelope;
+import static spring.skills.SpringSkillsTestHelpers.buildTestSession;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+
+import org.junit.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.context.annotation.UserConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazon.speech.json.SpeechletRequestEnvelope;
+import com.amazon.speech.json.SpeechletResponseEnvelope;
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SpeechletRequest;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.SimpleCard;
+
+import spring.skills.autoconfig.SpringSkillsAutoConfiguration;
+import spring.skills.handler.UnknownIntentHandler;
+
 public class IntentDispatcherTests {
 	
-	//
-	// TODO: Context runner stuff not available until Boot 2.0. Meanwhile,
-	//       Boot 2.0 makes stuff break WRT Spring Cloud Function, since 
-	//       SCF is built with 1.5.9.RELEASE. So...temporarily commenting out
-	//       tests. (They do pass if building against Boot 2.0.)
-	//
+	private final ApplicationContextRunner contextRunner = 
+			new ApplicationContextRunner()
+				.withConfiguration(
+						AutoConfigurations.of(SpringSkillsAutoConfiguration.class));
 	
+	@Test
+	public void shouldHandleSimpleIntent() {
+		contextRunner
+		.withConfiguration(UserConfigurations.of(IntentTestConfiguration.class))
+		.run(
+				context -> {
+					IntentDispatcher dispatcher = context.getBean(IntentDispatcher.class);
+					Intent intent = Intent.builder().withName("SayHello").build();
+					SpeechletResponseEnvelope responseEnv = dispatcher.handleIntentRequest(getIntentRequestEnvelope(intent));
+					
+					PlainTextOutputSpeech outputSpeech = 
+							(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
+					assertEquals("Hello world!", outputSpeech.getText());
+				}
+			);		
+	}
 	
-//	private final ApplicationContextRunner contextRunner = 
-//			new ApplicationContextRunner()
-//				.withConfiguration(
-//						AutoConfigurations.of(SpringSkillsAutoConfiguration.class));
-//	
-//	@Test
-//	public void shouldHandleSimpleIntent() {
-//		contextRunner
-//		.withConfiguration(UserConfigurations.of(IntentTestConfiguration.class))
-//		.run(
-//				context -> {
-//					IntentDispatcher dispatcher = context.getBean(IntentDispatcher.class);
-//					Intent intent = Intent.builder().withName("SayHello").build();
-//					SpeechletResponseEnvelope responseEnv = dispatcher.handleIntentRequest(getIntentRequestEnvelope(intent));
-//					
-//					PlainTextOutputSpeech outputSpeech = 
-//							(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
-//					assertEquals("Hello world!", outputSpeech.getText());
-//				}
-//			);		
-//	}
-//	
-//	@Test
-//	public void shouldHandleSimpleIntentWithAlternateNaming() {
-//		contextRunner
-//		.withConfiguration(UserConfigurations.of(IntentTestConfiguration.class))
-//		.run(
-//				context -> {
-//					IntentDispatcher dispatcher = context.getBean(IntentDispatcher.class);
-//					Intent intent = Intent.builder().withName("say_hello").build();
-//					SpeechletResponseEnvelope responseEnv = dispatcher.handleIntentRequest(getIntentRequestEnvelope(intent));
-//					
-//					PlainTextOutputSpeech outputSpeech = 
-//							(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
-//					assertEquals("Hello world!", outputSpeech.getText());
-//				}
-//			);	
-//	}
-//	
-//	@Test
-//	public void shouldHandleUnknownIntentWithDefaultHandler() {
-//		contextRunner.run(
-//				context -> {
-//					IntentDispatcher dispatcher = context.getBean(IntentDispatcher.class);
-//					Intent intent = Intent.builder().withName("BogusIntent").build();
-//					SpeechletResponseEnvelope responseEnv = dispatcher.handleIntentRequest(getIntentRequestEnvelope(intent));
-//					
-//					PlainTextOutputSpeech outputSpeech = 
-//							(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
-//					assertEquals("There was an error: Unknown intent.", outputSpeech.getText());
-//				}
-//			);	
-//	}
-//	
-//	@Test
-//	public void shouldHandleUnknownIntentWithCustomHandler() {
-//		contextRunner
-//		.withConfiguration(UserConfigurations.of(IntentTestConfiguration.class))
-//		.run(
-//				context -> {
-//					IntentDispatcher dispatcher = context.getBean(IntentDispatcher.class);
-//					Intent intent = Intent.builder().withName("BogusIntent").build();
-//					SpeechletResponseEnvelope responseEnv = dispatcher.handleIntentRequest(getIntentRequestEnvelope(intent));
-//					
-//					PlainTextOutputSpeech outputSpeech = 
-//							(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
-//					assertEquals("I beg your pardon.", outputSpeech.getText());
-//				}
-//			);	
-//	}
-//
-//	//
-//	// private test helpers
-//	//
-//	private SpeechletRequestEnvelope getIntentRequestEnvelope(Intent intent) {
-//		SpeechletRequest request = 
-//				IntentRequest.builder()
-//					.withIntent(intent)
-//					.withRequestId("request_1")
-//					.withTimestamp(new Date())
-//					.build();
-//		Session session = buildTestSession();
-//		return buildTestRequestEnvelope(request, session);
-//	}	
-//
-//	//
-//	// Test configuration
-//	//
-//	@Configuration
-//	public static class IntentTestConfiguration {
-//		@Bean(name={"say_hello", "sayHello"})
-//		public Function<SpeechletRequestEnvelope, SpeechletResponseEnvelope> say_hello() {
-//			return (req) -> {
-//				SpeechletResponseEnvelope responseEnv = new SpeechletResponseEnvelope();
-//				SpeechletResponse response = new SpeechletResponse();
-//				PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
-//				outputSpeech.setId("output_speech_1");
-//				outputSpeech.setText("Hello world!");
-//				response.setOutputSpeech(outputSpeech);
-//				responseEnv.setResponse(response);
-//				return responseEnv;
-//			};
-//		}
-//		
-//		@Bean
-//		public UnknownIntentHandler unknownIntentHandler() {
-//			return requestEnvelope -> {
-//				PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
-//				outputSpeech.setText("I beg your pardon.");
-//				SimpleCard card = new SimpleCard();
-//				card.setTitle("What?");
-//				card.setContent("I beg your pardon.");
-//				SpeechletResponse response = SpeechletResponse.newTellResponse(outputSpeech, card);
-//				SpeechletResponseEnvelope envelope = new SpeechletResponseEnvelope();
-//				envelope.setResponse(response);
-//				envelope.setSessionAttributes(new HashMap<>());
-//				envelope.setVersion("1.0");
-//				return envelope;
-//			};
-//		}
-//	}
+	@Test
+	public void shouldHandleSimpleIntentWithAlternateNaming() {
+		contextRunner
+		.withConfiguration(UserConfigurations.of(IntentTestConfiguration.class))
+		.run(
+				context -> {
+					IntentDispatcher dispatcher = context.getBean(IntentDispatcher.class);
+					Intent intent = Intent.builder().withName("say_hello").build();
+					SpeechletResponseEnvelope responseEnv = dispatcher.handleIntentRequest(getIntentRequestEnvelope(intent));
+					
+					PlainTextOutputSpeech outputSpeech = 
+							(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
+					assertEquals("Hello world!", outputSpeech.getText());
+				}
+			);	
+	}
+	
+	@Test
+	public void shouldHandleUnknownIntentWithDefaultHandler() {
+		contextRunner.run(
+				context -> {
+					IntentDispatcher dispatcher = context.getBean(IntentDispatcher.class);
+					Intent intent = Intent.builder().withName("BogusIntent").build();
+					SpeechletResponseEnvelope responseEnv = dispatcher.handleIntentRequest(getIntentRequestEnvelope(intent));
+					
+					PlainTextOutputSpeech outputSpeech = 
+							(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
+					assertEquals("There was an error: Unknown intent.", outputSpeech.getText());
+				}
+			);	
+	}
+	
+	@Test
+	public void shouldHandleUnknownIntentWithCustomHandler() {
+		contextRunner
+		.withConfiguration(UserConfigurations.of(IntentTestConfiguration.class))
+		.run(
+				context -> {
+					IntentDispatcher dispatcher = context.getBean(IntentDispatcher.class);
+					Intent intent = Intent.builder().withName("BogusIntent").build();
+					SpeechletResponseEnvelope responseEnv = dispatcher.handleIntentRequest(getIntentRequestEnvelope(intent));
+					
+					PlainTextOutputSpeech outputSpeech = 
+							(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
+					assertEquals("I beg your pardon.", outputSpeech.getText());
+				}
+			);	
+	}
+
+	//
+	// private test helpers
+	//
+	private SpeechletRequestEnvelope getIntentRequestEnvelope(Intent intent) {
+		SpeechletRequest request = 
+				IntentRequest.builder()
+					.withIntent(intent)
+					.withRequestId("request_1")
+					.withTimestamp(new Date())
+					.build();
+		Session session = buildTestSession();
+		return buildTestRequestEnvelope(request, session);
+	}	
+
+	//
+	// Test configuration
+	//
+	@Configuration
+	public static class IntentTestConfiguration {
+		@Bean(name={"say_hello", "sayHello"})
+		public Function<SpeechletRequestEnvelope, SpeechletResponseEnvelope> say_hello() {
+			return (req) -> {
+				SpeechletResponseEnvelope responseEnv = new SpeechletResponseEnvelope();
+				SpeechletResponse response = new SpeechletResponse();
+				PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+				outputSpeech.setId("output_speech_1");
+				outputSpeech.setText("Hello world!");
+				response.setOutputSpeech(outputSpeech);
+				responseEnv.setResponse(response);
+				return responseEnv;
+			};
+		}
+		
+		@Bean
+		public UnknownIntentHandler unknownIntentHandler() {
+			return requestEnvelope -> {
+				PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+				outputSpeech.setText("I beg your pardon.");
+				SimpleCard card = new SimpleCard();
+				card.setTitle("What?");
+				card.setContent("I beg your pardon.");
+				SpeechletResponse response = SpeechletResponse.newTellResponse(outputSpeech, card);
+				SpeechletResponseEnvelope envelope = new SpeechletResponseEnvelope();
+				envelope.setResponse(response);
+				envelope.setSessionAttributes(new HashMap<>());
+				envelope.setVersion("1.0");
+				return envelope;
+			};
+		}
+	}
 }
--- spring-skills/src/test/java/spring/skills/SpeechletRequestDispatcherTests.java
+++ spring-skills/src/test/java/spring/skills/SpeechletRequestDispatcherTests.java
@@ -15,198 +15,218 @@
  */
 package spring.skills;
 
+import static org.junit.Assert.assertEquals;
+import static spring.skills.SpringSkillsTestHelpers.getBogusRequest;
+import static spring.skills.SpringSkillsTestHelpers.getIntentRequest;
+import static spring.skills.SpringSkillsTestHelpers.getLaunchRequest;
+import static spring.skills.SpringSkillsTestHelpers.getSessionEndedRequest;
+
+import java.util.HashMap;
+import java.util.function.Function;
+
+import org.junit.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.context.annotation.UserConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazon.speech.json.SpeechletRequestEnvelope;
+import com.amazon.speech.json.SpeechletResponseEnvelope;
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.SimpleCard;
+
+import spring.skills.autoconfig.SpringSkillsAutoConfiguration;
+import spring.skills.handler.LaunchRequestHandler;
+import spring.skills.handler.SessionEndedRequestHandler;
+import spring.skills.handler.UnknownRequestHandler;
+
 public class SpeechletRequestDispatcherTests {
 
-	//
-	// TODO: Context runner stuff not available until Boot 2.0. Meanwhile,
-	//       Boot 2.0 makes stuff break WRT Spring Cloud Function, since 
-	//       SCF is built with 1.5.9.RELEASE. So...temporarily commenting out
-	//       tests. (They do pass if building against Boot 2.0.)
-	//
+	private final ApplicationContextRunner contextRunner = 
+			new ApplicationContextRunner()
+				.withConfiguration(
+						AutoConfigurations.of(SpringSkillsAutoConfiguration.class));
 
+	@Test
+	public void shouldHandleLaunchRequestWithDefaultResponse() {
+		contextRunner.run(
+			context -> {
+				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
+				SpeechletResponseEnvelope responseEnv = dispatcher.handle(getLaunchRequest());
+				PlainTextOutputSpeech outputSpeech = 
+						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
+				assertEquals("Welcome", outputSpeech.getText());
+			}
+		);
+	}
 	
-//	private final ApplicationContextRunner contextRunner = 
-//			new ApplicationContextRunner()
-//				.withConfiguration(
-//						AutoConfigurations.of(SpringSkillsAutoConfiguration.class));
-//
-//	@Test
-//	public void shouldHandleLaunchRequestWithDefaultResponse() {
-//		contextRunner.run(
-//			context -> {
-//				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
-//				SpeechletResponseEnvelope responseEnv = dispatcher.apply(getLaunchRequest());
-//				PlainTextOutputSpeech outputSpeech = 
-//						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
-//				assertEquals("Welcome", outputSpeech.getText());
-//			}
-//		);
-//	}
-//	
-//	@Test
-//	public void shouldHandleLaunchRequestWithOverridingRequestHandler() {
-//		contextRunner
-//		.withConfiguration(UserConfigurations.of(CustomSpringSkillConfiguration.class))
-//		.run(
-//			context -> {
-//				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
-//				SpeechletResponseEnvelope responseEnv = dispatcher.apply(getLaunchRequest());
-//				PlainTextOutputSpeech outputSpeech = 
-//						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
-//				assertEquals("Buenos dias", outputSpeech.getText());
-//			}
-//		);
-//	}
-//	
-//	@Test
-//	public void shouldHandleSessionEndedRequestWithDefaultResponse() {
-//		contextRunner.run(
-//			context -> {
-//				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
-//				SpeechletResponseEnvelope responseEnv = dispatcher.apply(getSessionEndedRequest());
-//				PlainTextOutputSpeech outputSpeech = 
-//						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
-//				assertEquals("Goodbye", outputSpeech.getText());
-//			}
-//		);
-//	}
-//
-//	@Test
-//	public void shouldHandleSessionEndedRequestWithOverridingRequestHandler() {
-//		contextRunner.run(
-//			context -> {
-//				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
-//				SpeechletResponseEnvelope responseEnv = dispatcher.apply(getSessionEndedRequest());
-//				PlainTextOutputSpeech outputSpeech = 
-//						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
-//				assertEquals("Goodbye", outputSpeech.getText());
-//			}
-//		);
-//	}
-//
-//	@Test
-//	public void shouldHandleUnrecognizedRequestWithDefaultResponse() {
-//		contextRunner
-//		.run(
-//			context -> {
-//				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
-//				SpeechletResponseEnvelope responseEnv = dispatcher.apply(getBogusRequest());
-//				PlainTextOutputSpeech outputSpeech = 
-//						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
-//				assertEquals("There was an error: Unknown request type."
-//						, outputSpeech.getText());			}
-//		);
-//	}
-//	
-//	@Test
-//	public void shouldHandleUnrecognizedRequestWithOverridingRequestHandler() {
-//		contextRunner.withConfiguration(UserConfigurations.of(CustomSpringSkillConfiguration.class))
-//		.run(
-//			context -> {
-//				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
-//				SpeechletResponseEnvelope responseEnv = dispatcher.apply(getBogusRequest());
-//				PlainTextOutputSpeech outputSpeech = 
-//						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
-//				assertEquals("I have no idea what you're asking for."
-//						, outputSpeech.getText());			
-//			}
-//		);
-//	}
-//	
-//	@Test
-//	public void shouldHandleIntentRequest() {
-//		contextRunner.withConfiguration(UserConfigurations.of(CustomSpringSkillConfiguration.class))
-//		.run(
-//			context -> {
-//				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
-//				Intent intent = Intent.builder()
-//						.withName("hello")
-//						.build();
-//				SpeechletResponseEnvelope responseEnv = dispatcher.apply(getIntentRequest(intent));
-//				PlainTextOutputSpeech outputSpeech = 
-//						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
-//				assertEquals("Hello Spring World", outputSpeech.getText());			
-//			}
-//		);
-//	}
-//	 
-//	@Configuration
-//  	public static class CustomSpringSkillConfiguration {
-//		
-//		@Bean
-//		public Function<SpeechletRequestEnvelope, SpeechletResponseEnvelope> hello() {
-//			return (requestEnvelope) -> {
-//				SpeechletResponseEnvelope responseEnvelope = new SpeechletResponseEnvelope();
-//				SpeechletResponse response = new SpeechletResponse();
-//				SimpleCard card = new SimpleCard();
-//				card.setTitle("Hello");
-//				card.setContent("Hello Spring World");
-//				response.setCard(card);
-//				PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
-//				outputSpeech.setId("hello");
-//				outputSpeech.setText("Hello Spring World");
-//				response.setOutputSpeech(outputSpeech);
-//				responseEnvelope.setResponse(response);
-//				return responseEnvelope;
-//			};
-//		}
-//		
-//		@Bean
-//		public LaunchRequestHandler launchRequestHandler() {
-//			return (requestEnvelope) -> {
-//				SpeechletResponseEnvelope responseEnvelope = new SpeechletResponseEnvelope();
-//				SpeechletResponse response = new SpeechletResponse();
-//				SimpleCard card = new SimpleCard();
-//				card.setTitle("Buenos dias");
-//				card.setContent("Buenos dias");
-//				response.setCard(card);
-//				PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
-//				outputSpeech.setId("buenos_dias");
-//				outputSpeech.setText("Buenos dias");
-//				response.setOutputSpeech(outputSpeech);
-//				responseEnvelope.setResponse(response);
-//				return responseEnvelope;
-//			};
-//		}
-//		
-//		@Bean
-//		public SessionEndedRequestHandler sessionEndedRequestHandler() {
-//			return (requestEnvelope) -> {
-//				SpeechletResponseEnvelope responseEnvelope = new SpeechletResponseEnvelope();
-//				SpeechletResponse response = new SpeechletResponse();
-//				SimpleCard card = new SimpleCard();
-//				card.setTitle("Adios");
-//				card.setContent("Adios");
-//				response.setCard(card);
-//				PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
-//				outputSpeech.setId("adios");
-//				outputSpeech.setText("Adios");
-//				response.setOutputSpeech(outputSpeech);
-//				responseEnvelope.setResponse(response);
-//				return responseEnvelope;
-//			};
-//		}
-//		
-//		@Bean
-//		public UnknownRequestHandler unknownRequestHandler() {
-//			return (requestEnvelope) -> {
-//				PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
-//				outputSpeech.setText("I have no idea what you're asking for.");
-//	
-//				SimpleCard card = new SimpleCard();
-//				card.setTitle("Spring Skills Error");
-//				card.setContent("I have no idea what you're asking for.");
-//	
-//				SpeechletResponse response = SpeechletResponse.newTellResponse(outputSpeech, card);
-//				SpeechletResponseEnvelope envelope = new SpeechletResponseEnvelope();
-//				envelope.setResponse(response);
-//				envelope.setSessionAttributes(new HashMap<>());
-//				envelope.setVersion("1.0");
-//	
-//				return envelope;
-//			};
-//		}
-//		
-//	}
+	@Test
+	public void shouldHandleLaunchRequestWithOverridingRequestHandler() {
+		contextRunner
+		.withConfiguration(UserConfigurations.of(CustomSpringSkillConfiguration.class))
+		.run(
+			context -> {
+				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
+				SpeechletResponseEnvelope responseEnv = dispatcher.handle(getLaunchRequest());
+				PlainTextOutputSpeech outputSpeech = 
+						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
+				assertEquals("Buenos dias", outputSpeech.getText());
+			}
+		);
+	}
+	
+	@Test
+	public void shouldHandleSessionEndedRequestWithDefaultResponse() {
+		contextRunner.run(
+			context -> {
+				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
+				SpeechletResponseEnvelope responseEnv = dispatcher.handle(getSessionEndedRequest());
+				PlainTextOutputSpeech outputSpeech = 
+						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
+				assertEquals("Goodbye", outputSpeech.getText());
+			}
+		);
+	}
+
+	@Test
+	public void shouldHandleSessionEndedRequestWithOverridingRequestHandler() {
+		contextRunner.run(
+			context -> {
+				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
+				SpeechletResponseEnvelope responseEnv = dispatcher.handle(getSessionEndedRequest());
+				PlainTextOutputSpeech outputSpeech = 
+						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
+				assertEquals("Goodbye", outputSpeech.getText());
+			}
+		);
+	}
+
+	@Test
+	public void shouldHandleUnrecognizedRequestWithDefaultResponse() {
+		contextRunner
+		.run(
+			context -> {
+				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
+				SpeechletResponseEnvelope responseEnv = dispatcher.handle(getBogusRequest());
+				PlainTextOutputSpeech outputSpeech = 
+						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
+				assertEquals("There was an error: Unknown request type."
+						, outputSpeech.getText());			}
+		);
+	}
+	
+	@Test
+	public void shouldHandleUnrecognizedRequestWithOverridingRequestHandler() {
+		contextRunner.withConfiguration(UserConfigurations.of(CustomSpringSkillConfiguration.class))
+		.run(
+			context -> {
+				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
+				SpeechletResponseEnvelope responseEnv = dispatcher.handle(getBogusRequest());
+				PlainTextOutputSpeech outputSpeech = 
+						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
+				assertEquals("I have no idea what you're asking for."
+						, outputSpeech.getText());			
+			}
+		);
+	}
+	
+	@Test
+	public void shouldHandleIntentRequest() {
+		contextRunner.withConfiguration(UserConfigurations.of(CustomSpringSkillConfiguration.class))
+		.run(
+			context -> {
+				SpeechletRequestDispatcher dispatcher = context.getBean(SpeechletRequestDispatcher.class);
+				Intent intent = Intent.builder()
+						.withName("hello")
+						.build();
+				SpeechletResponseEnvelope responseEnv = dispatcher.handle(getIntentRequest(intent));
+				PlainTextOutputSpeech outputSpeech = 
+						(PlainTextOutputSpeech) responseEnv.getResponse().getOutputSpeech();
+				assertEquals("Hello Spring World", outputSpeech.getText());			
+			}
+		);
+	}
+	 
+	@Configuration
+  	public static class CustomSpringSkillConfiguration {
+		
+		@Bean
+		public Function<SpeechletRequestEnvelope, SpeechletResponseEnvelope> hello() {
+			return (requestEnvelope) -> {
+				SpeechletResponseEnvelope responseEnvelope = new SpeechletResponseEnvelope();
+				SpeechletResponse response = new SpeechletResponse();
+				SimpleCard card = new SimpleCard();
+				card.setTitle("Hello");
+				card.setContent("Hello Spring World");
+				response.setCard(card);
+				PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+				outputSpeech.setId("hello");
+				outputSpeech.setText("Hello Spring World");
+				response.setOutputSpeech(outputSpeech);
+				responseEnvelope.setResponse(response);
+				return responseEnvelope;
+			};
+		}
+		
+		@Bean
+		public LaunchRequestHandler launchRequestHandler() {
+			return (requestEnvelope) -> {
+				SpeechletResponseEnvelope responseEnvelope = new SpeechletResponseEnvelope();
+				SpeechletResponse response = new SpeechletResponse();
+				SimpleCard card = new SimpleCard();
+				card.setTitle("Buenos dias");
+				card.setContent("Buenos dias");
+				response.setCard(card);
+				PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+				outputSpeech.setId("buenos_dias");
+				outputSpeech.setText("Buenos dias");
+				response.setOutputSpeech(outputSpeech);
+				responseEnvelope.setResponse(response);
+				return responseEnvelope;
+			};
+		}
+		
+		@Bean
+		public SessionEndedRequestHandler sessionEndedRequestHandler() {
+			return (requestEnvelope) -> {
+				SpeechletResponseEnvelope responseEnvelope = new SpeechletResponseEnvelope();
+				SpeechletResponse response = new SpeechletResponse();
+				SimpleCard card = new SimpleCard();
+				card.setTitle("Adios");
+				card.setContent("Adios");
+				response.setCard(card);
+				PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+				outputSpeech.setId("adios");
+				outputSpeech.setText("Adios");
+				response.setOutputSpeech(outputSpeech);
+				responseEnvelope.setResponse(response);
+				return responseEnvelope;
+			};
+		}
+		
+		@Bean
+		public UnknownRequestHandler unknownRequestHandler() {
+			return (requestEnvelope) -> {
+				PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+				outputSpeech.setText("I have no idea what you're asking for.");
+	
+				SimpleCard card = new SimpleCard();
+				card.setTitle("Spring Skills Error");
+				card.setContent("I have no idea what you're asking for.");
+	
+				SpeechletResponse response = SpeechletResponse.newTellResponse(outputSpeech, card);
+				SpeechletResponseEnvelope envelope = new SpeechletResponseEnvelope();
+				envelope.setResponse(response);
+				envelope.setSessionAttributes(new HashMap<>());
+				envelope.setVersion("1.0");
+	
+				return envelope;
+			};
+		}
+		
+	}
 
 }
