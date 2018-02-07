9c9dbbda3777c8cc2d7cc7f71b23e945823ffac8
Craig Walls <craig@habuma.com>
2018-02-06 22:31:44 -0700

    Auto-configure function.name property

--- spring-skills/src/main/java/spring/skills/autoconfig/SpringSkillsAutoConfiguration.java
+++ spring-skills/src/main/java/spring/skills/autoconfig/SpringSkillsAutoConfiguration.java
@@ -20,6 +20,7 @@ import java.util.function.Function;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import com.amazon.speech.json.SpeechletRequestEnvelope;
 import com.amazon.speech.json.SpeechletResponseEnvelope;
@@ -37,16 +38,16 @@ import spring.skills.handler.UnknownIntentHandler;
 import spring.skills.handler.UnknownRequestHandler;
 
 @Configuration
+@PropertySource("classpath:spring/skills/autoconfig/skills.properties")
 public class SpringSkillsAutoConfiguration {
 	
 	@Bean
 	public SpringSkillsProperties springSkillsProperties() {
 		return new SpringSkillsProperties();
 	}
-	
 
-	@Bean("requestDispatcher")
-	public Function<SpeechletRequestEnvelope, SpeechletResponseEnvelope> srd() {
+	@Bean
+	public Function<SpeechletRequestEnvelope, SpeechletResponseEnvelope> _requestDispatcher() {
 		return requestEnv -> {
 			return _speechletRequestDispatcher().handle(requestEnv);
 		};
